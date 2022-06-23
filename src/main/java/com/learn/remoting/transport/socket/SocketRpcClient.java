cdbf2dc781f23ea6a817e2eae349286a046a58d5
sunyh <1915257388@qq.com>
2022-06-23 21:56:09 +0800

    add blockingqueue

---
+++ src/main/java/com/learn/remoting/transport/socket/SocketRpcClient.java
@@ -0,0 +1,12 @@
+package com.learn.remoting.transport.socket;
+
+import com.learn.remoting.dto.RpcRequest;
+
+import java.net.ServerSocket;
+
+public class SocketRpcClient {
+    public static void main(String[] args) {
+        RpcRequest request = new RpcRequest();
+
+    }
+}
--- src/main/java/com/learn/remoting/transport/socket/SocketRpcServer.java
+++ src/main/java/com/learn/remoting/transport/socket/SocketRpcServer.java
@@ -5,6 +5,8 @@ import com.learn.remoting.dto.RpcRequest;
 import sun.nio.ch.ThreadPool;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -30,7 +32,19 @@ public class SocketRpcServer implements RpcRequestTransport {
             serverSocket.bind(new InetSocketAddress(port));
             Socket socket;
             while((socket = serverSocket.accept())!=null){
-                threadPool.execute();
+                InputStream inputStream = socket.getInputStream();
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                RpcRequest request = null;
+                try {
+                    request = (RpcRequest)objectInputStream.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if(request!=null) {
+                    request.getMethodName();
+                }
+
+
             }
 
         } catch (IOException e) {
